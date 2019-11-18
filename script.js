3fa7daba66315bc46f3313b923bd68541204bf8b
joharbatta <joharbatta@gmail.com>
2019-11-19 03:16:01 +0530

    design align and some changes

--- script.js
+++ script.js
@@ -188,10 +188,16 @@ function execute()
 	
 	
 	
-	// When the user clicks on <span> (x), close the modal
-	span.onclick = function() {
-	  modal.style.display = "none";
-	}
+						// When the user clicks on <span> (x), close the modal
+						span.onclick = function() {
+						modal.style.display = "none";
+						}
+						window.onclick = function(event) {
+							if (event.target == modal) {
+							modal.style.display = "none";
+							}
+						}
+	
 	
 	
 	
@@ -207,6 +213,7 @@ function execute()
 
 
 }
+
 
 
 
